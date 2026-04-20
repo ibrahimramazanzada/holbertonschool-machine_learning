7fa1165f4e769037e41aabb9143e5e07aa03280d
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-20 14:00:13 +0000

    math

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,4 +4,8 @@
 
 def poly_derivative(poly):
     """Calculate derivative of polynomial"""
+    if not isinstance(poly, list):
+        return None
+    if len(poly) < 2:
+        return [0]
     return [coef * exp for exp, coef in enumerate(poly)][1:]
