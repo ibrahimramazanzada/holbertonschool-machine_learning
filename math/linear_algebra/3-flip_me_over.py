a59214b1f7a919a66d84f92e75a237a5246bb24f
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-27 22:24:16 +0400

    linalg

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""""This does smth fr"""
+"""This does smth fr"""
 
 
 def matrix_transpose(matrix):
-    """"Transposes a matrix"""
+    """Transposes a matrix"""
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
