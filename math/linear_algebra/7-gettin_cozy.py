c1f5e68935103c09f09450a212855ee6a9bf2f4b
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-30 17:35:33 +0400

    linalg

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,8 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """"concats matrices"""
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
     elif axis == 1:
         if len(mat1) != len(mat2):
