6758b4ecf9e4174d225a51521afecfac8f6ebb34
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-02 22:57:43 +0400

    fisher

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -3,25 +3,23 @@
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """Concats"""
-    if not isinstance(mat1, list) or not isinstance(mat2, list):
-        return None
+    """"Concatenates"""
     if axis == 0:
-            if isinstance(mat1, list) and isinstance(mat2, list):
-                if isinstance(mat1[0], list) and isinstance(mat2[0], list):
-                    if len(mat1[0]) != len(mat2[0]):
-                        return None
-                return mat1 + mat2
-            return None
-    else:
-        if len(mat1) != len(mat2):
-            return None
+        if isinstance(mat1, list) and isinstance(mat2, list):
+            if isinstance(mat1[0], list) and isinstance(mat2[0], list):
+                if len(mat1[0]) != len(mat2[0]):
+                    return None
+            return mat1 + mat2
+        return None
 
-        result = []
-        for i in range(len(mat1)):
-            concatenated = cat_matrices(mat1[i], mat2[i], axis - 1)
-            if concatenated is None:
-                return None
-            result.append(concatenated)
-        return result
+    if not isinstance(mat1, list) or not isinstance(mat2, list) or len(mat1) != len(mat2):
+        return None
+
+    new_matrix = []
+    for i in range(len(mat1)):
+        res = cat_matrices(mat1[i], mat2[i], axis - 1)
+        if res is None:
+            return None
+        new_matrix.append(res)
     
+    return new_matrix
