ee3dad2e9aa1012921ec34c14d247ad0d5d0bb01
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-02 22:08:28 +0400

    ninja

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,10 @@
+##!usr/bin/env python3
+"""W-watch out"""
+
+
+def np_slice(matrix, axes={}):
+    """slices"""
+    slices = [slice(None)] * len(matrix.shape)
+    for axis, slice_tuple in axes.items():
+        slices[axis] = slice(*slice_tuple)
+    return matrix[tuple(slices)]
