dcb6c2cbc8ece5df2c490246343b276b40503432
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-15 12:29:35 +0400

    poisson

---
+++ math/probability/exponential.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Exponential distribution."""
+
+
+class Exponential:
+    """Exponential distribution."""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initialize"""
+
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(len(data) / sum(data))
+
+    def pdf(self, x):
+        """PDF of x"""
+
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        return self.lambtha * (e ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """CDF of x"""
+
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        return 1 - (e ** (-self.lambtha * x))
