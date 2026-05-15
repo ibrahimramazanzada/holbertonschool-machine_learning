1bd07b54ae83581044480a85a256269db7c4e6fb
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-15 14:08:00 +0400

    normal

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -25,3 +25,22 @@ class Binomial:
             self.p = 1 - (variance / mean)
             self.n = int(round(mean / self.p))
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        """PMF of k"""
+
+        if k < 0 or k > self.n:
+            return 0
+        k = int(k)
+        p_k = self.p ** k
+        q_n_k = (1 - self.p) ** (self.n - k)
+        n_k_factorial = 1
+        for i in range(1, self.n - k + 1):
+            n_k_factorial *= i
+        k_factorial = 1
+        for i in range(1, k + 1):
+            k_factorial *= i
+        n_factorial = 1
+        for i in range(1, self.n + 1):
+            n_factorial *= i
+        return (n_factorial / (k_factorial * n_k_factorial)) * p_k * q_n_k
