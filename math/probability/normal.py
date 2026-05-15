b18b4730d7988ef8e9a8bbd192cc0f00cdb1a714
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-15 12:38:22 +0400

    poisson

---
+++ math/probability/normal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Normal distribution."""
+
+
+class Normal:
+    """Normal distribution."""
+
+    def __init__(self, data=None, mean=0, stddev=1):
+        """Initialize"""
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            self.stddev = float((sum((x - self.mean) ** 2 for x in data) /
+                                 len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """Z-score of x"""
+
+        return (x - self.mean) / self.stddev if self.stddev else 0
+
+    def x_value(self, z):
+        """X-value of z"""
+
+        return self.mean + z * self.stddev
