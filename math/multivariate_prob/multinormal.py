2da7c93117ef17076798906b5e00d3b1fd24a412
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-22 22:56:36 +0400

    cov

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -20,3 +20,21 @@ class MultiNormal:
         self.cov = (np.matmul((data.T - self.mean).T, (data.T - self.mean)) /
                     (n - 1))
         self.mean = self.mean.T
+
+    def pdf(self, x):
+        """calculates the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({d}, 1)".format(d=d))
+
+        d = self.mean.shape[0]
+        cov_inv = np.linalg.inv(self.cov)
+        cov_det = np.linalg.det(self.cov)
+
+        norm_const = 1.0 / (np.power(2 * np.pi, d / 2) * np.sqrt(cov_det))
+        x_diff = x - self.mean
+        exponent = -0.5 * np.dot(np.dot(x_diff.T, cov_inv), x_diff)
+
+        return float((norm_const * np.exp(exponent))[0][0])
