4f1db004572483f83672682228a8b705b5685947
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-27 23:27:20 +0400

    pandas

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''concatenating two pd.DataFrames'''
+
+
+def concat(df1, df2):
+    '''returns a concatenated pd.DataFrame'''
+    df1 = df1.set_index('Timestamp')
+    df2 = df2.set_index('Timestamp')
+    df2 = df2.loc[:1417411920]
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    return df
