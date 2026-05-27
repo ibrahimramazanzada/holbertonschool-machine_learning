d9f3fff93a7756223a6fb0f33f2231a2232af276
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-27 23:38:10 +0400

    pandas

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-'''swapping levels of a hierarchical index'''
+"""swapping levels of a hierarchical index"""
 import pandas as pd
 index = __import__('10-index').index
 
 
 def hierarchy(df1, df2):
-    '''returns a pd.DataFrame with the levels of the hierarchical index swapped'''
+    """returns a pd.DataFrame with the levels of the hierarchical index swapped"""
     df1 = df1.set_index('Timestamp')
     df2 = df2.set_index('Timestamp')
+    
     df2 = df2.loc[1417411980:1417417980]
     df1 = df1.loc[1417411980:1417417980]
+    
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-    df.swaplevel(0, 1, inplace=True)
-    df.sort_index(inplace=True)
+    
+    # Swap levels and sort index via assignment instead of inplace=True
+    df = df.swaplevel(0, 1)
+    df = df.sort_index()
+    
     return df
