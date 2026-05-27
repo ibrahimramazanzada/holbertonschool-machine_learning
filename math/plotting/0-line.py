421b3eabfd18af30882615e3b45b4eef37fe31a7
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-28 00:37:54 +0400

    pandas

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,6 +7,7 @@ def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 11)
-    plt.plot(x, y, color='red')
+    x = np.arange(0, 10)
+    plt.plot(y, color='red')
+    plt.xlim(0, 10)
     plt.show()
