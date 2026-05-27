a491e7891f5e28ae797e97eca7714ab7ba8a8bc4
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-28 00:46:07 +0400

    pandas

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''change scale of plot'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    ''''change scale of plot'''
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    plt.show()
