1c94a1e9f3cde681d02dd98cc0b687ae1010f348
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-27 21:25:32 +0400

    pandas

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''slicing a pd.DataFrame'''
+
+
+def slice(df):
+    '''returns a slice of a pd.DataFrame'''
+    df2 = df.loc[:, ['High', 'Low', 'Close', 'Volume_BTC']]
+    return df2.iloc[59, :]
