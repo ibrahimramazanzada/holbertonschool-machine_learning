bbffa2191718e54ae5edadb2e524d3cddaee0f3b
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-28 00:39:40 +0400

    pandas

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
+'''line plot'''
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    ''''line graph'''
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 10)
     plt.plot(y, color='red')
     plt.xlim(0, 10)
     plt.show()
