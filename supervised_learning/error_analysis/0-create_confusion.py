8a095947c7832a5bfd4bcccdbfaaf360ef2bad21
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-06-22 15:15:19 +0400

    confusion

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,19 @@
+##!/usr/bin/env python3
+'''Creating confusion matrix from labels and logits.'''
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    '''Create a confusion matrix from labels and logits.'''
+    predicted = np.argmax(logits, axis=1)
+    true = np.argmax(labels, axis=1)
+    num_classes = logits.shape[1]
+
+    confusion_matrix = np.zeros((num_classes, num_classes), dtype=int)
+
+    for i in range(len(true)):
+        true_class = true[i]
+        predicted_class = predicted[i]
+        confusion_matrix[true_class, predicted_class] += 1
+
+    return confusion_matrix
