36fb904e54054ffda97e1350e49b2d46a67bcaf5
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-06-27 23:32:46 +0400

    trees

---
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''Random Forest to detect outliers using Isolation Trees'''
+import numpy as np
+import matplotlib.pyplot as plt
+Isolation_Random_Tree = __import__('10-isolation_tree').Isolation_Random_Tree
+
+
+class Isolation_Random_Forest():
+    '''Isolation Random Forest Classifier'''
+    def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        '''Initializes the isolation random forest classifier'''
+        self.numpy_predicts = []
+        self.target = None
+        self.numpy_preds = None
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.seed = seed
+
+    def predict(self, explanatory):
+        '''Returns the mean depth of each individual'''
+        predictions = np.array([f(explanatory) for f in self.numpy_preds])
+        return predictions.mean(axis=0)
+
+    def fit(self, explanatory, n_trees=100, verbose=0):
+        '''Fits the isolation random forest to the explanatory'''
+        self.explanatory = explanatory
+        self.numpy_preds = []
+        depths = []
+        nodes = []
+        leaves = []
+        for i in range(n_trees):
+            T = Isolation_Random_Tree(max_depth=self.max_depth,
+                                      seed=self.seed+i)
+            T.fit(explanatory)
+            self.numpy_preds.append(T.predict)
+            depths.append(T.depth())
+            nodes.append(T.count_nodes())
+            leaves.append(T.count_nodes(only_leaves=True))
+        if verbose == 1:
+            print(f"""  Training finished.
+    - Mean depth                     : {np.array(depths).mean()}
+    - Mean number of nodes           : {np.array(nodes).mean()}
+    - Mean number of leaves          : {np.array(leaves).mean()}""")
+
+    def suspects(self, explanatory, n_suspects):
+        '''Returns the n_suspects rows with smallest mean depth and depths.'''
+        depths = self.predict(explanatory)
+        sorted_indices = np.argsort(depths)[:n_suspects]
+        return explanatory[sorted_indices], depths[sorted_indices]
--- supervised_learning/decision_tree/tempCodeRunnerFile.py
+++ supervised_learning/decision_tree/tempCodeRunnerFile.py
@@ -1,4 +1,5 @@
-    - Depth                     : {self.depth()}
-    - Number of nodes           : {self.count_nodes()}
-    - Number of leaves          : {self.count_nodes(only_leaves=True)}
-    - Accuracy on training data : {self.accuracy(self.explanatory, self.target)}
+    def suspects(self, explanatory, n_suspects):
+        '''Returns the n_suspects rows with smallest mean depth and their depths.'''
+        depths = self.predict(explanatory)
+        sorted_indices = np.argsort(depths)[:n_suspects]
+        return explanatory[sorted_indices], depths[sorted_indices]
