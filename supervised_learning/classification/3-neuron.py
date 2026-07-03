c83dc949140c6e2b5ec5516247847982d6c0d57c
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-03 22:17:37 +0400

    neural

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -43,3 +43,10 @@ class Neuron():
         cost = - (1 / m) * np.sum(Y * np.log(A) +
                                   (1 - Y) * np.log(1.0000001 - A))
         return cost
+
+    def evaluate(self, X, Y):
+        '''Evaluates the neuron’s predictions'''
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
