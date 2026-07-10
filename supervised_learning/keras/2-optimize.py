5680174c4b174504072a615a3c7c56bb6cdf6108
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-10 23:59:15 +0400

    keras

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Input model implementation with keras'''
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    '''Sets up Adam optimization for a keras model'''
+    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1,
+                                  beta_2=beta2)
+    network.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    return None
