521cfe5c050bf838f0490da75f3542a84b305484
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 00:04:33 +0400

    keras

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Input model implementation with keras'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    '''Trains a model using mini-batch gradient descent'''
+    history = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return history
