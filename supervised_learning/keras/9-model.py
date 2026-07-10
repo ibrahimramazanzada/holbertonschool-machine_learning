00b6ac80609ede88ededa61f4fa18bc4a176ad32
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 01:02:11 +0400

    keras

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -7,6 +7,7 @@ def save_model(network, filename):
     '''Saves an entire model'''
     network.save(filename)
 
+
 def load_model(filename):
     '''Loads an entire model'''
     return K.models.load_model(filename)
