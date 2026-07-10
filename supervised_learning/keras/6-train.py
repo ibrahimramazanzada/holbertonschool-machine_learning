10872802a532dbbee2d2525874b1c4336b1b3e16
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 00:13:48 +0400

    keras

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -7,8 +7,12 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
     '''Trains a model using mini-batch gradient descent'''
+    if validation_data and early_stopping:
+        early_stopping = K.callbacks.EarlyStopping(monitor='val_loss',
+                                                   patience=patience)
     history = network.fit(x=data, y=labels, batch_size=batch_size,
                           epochs=epochs, validation_data=validation_data,
-                          early_stopping=early_stopping, patience=patience,
+                          callbacks=[early_stopping]
+                          if early_stopping else None,
                           verbose=verbose, shuffle=shuffle)
     return history
