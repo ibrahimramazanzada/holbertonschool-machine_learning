2a2e1c2cce7ca64ade368e05a15fe028beb3a8c2
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 00:09:51 +0400

    keras

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -4,9 +4,11 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, verbose=True, shuffle=False):
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
     '''Trains a model using mini-batch gradient descent'''
     history = network.fit(x=data, y=labels, batch_size=batch_size,
                           epochs=epochs, validation_data=validation_data,
+                          early_stopping=early_stopping, patience=patience,
                           verbose=verbose, shuffle=shuffle)
     return history
