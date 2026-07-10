091213fda11062ce7642e88660ea4c01ff32271c
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 00:40:59 +0400

    keras

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -8,19 +8,19 @@ def train_model(network, data, labels, batch_size, epochs,
                 learning_rate_decay=False, alpha=0.1, decay_rate=1,
                 verbose=True, shuffle=False):
     '''Trains a model using mini-batch gradient descent'''
+    callbacks = []
     if validation_data and early_stopping:
         early_stopping = K.callbacks.EarlyStopping(monitor='val_loss',
                                                    patience=patience)
+        callbacks.append(early_stopping)
     if validation_data and learning_rate_decay:
         def scheduler(epoch):
             return alpha / (1 + decay_rate * epoch)
         learning_rate_decay = K.callbacks.LearningRateScheduler(scheduler,
                                                                 verbose=1)
+        callbacks.append(learning_rate_decay)
     history = network.fit(x=data, y=labels, batch_size=batch_size,
                           epochs=epochs, validation_data=validation_data,
-                          callbacks=[early_stopping]
-                          if early_stopping and validation_data else None,
-                          learning_rate_decay=learning_rate_decay
-                          if learning_rate_decay and validation_data else None,
+                          callbacks=callbacks if callbacks else None,
                           verbose=verbose, shuffle=shuffle)
     return history
