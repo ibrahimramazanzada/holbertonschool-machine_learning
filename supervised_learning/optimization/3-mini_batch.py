530cd26608c14a10fbbe824300e656b733856c02
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 23:50:45 +0400

    optima

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -14,9 +14,6 @@ def mini_batch(X, Y, batch_size):
     mini_batches_Y = []
     for i in range(0, X.shape[0], batch_size):
         if i + batch_size > X.shape[0]:
-            mini_batches_X.append(X_shuffled[i:X.shape[0]])
-            mini_batches_Y.append(Y_shuffled[i:Y.shape[0]])
-        else:
             mini_batches_X.append(X_shuffled[i:i + batch_size])
             mini_batches_Y.append(Y_shuffled[i:i + batch_size])
     return mini_batches_X, mini_batches_Y
