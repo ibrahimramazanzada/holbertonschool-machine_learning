ba82a4c13231efb420f190003c6b7b028aa9e7c4
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-12 00:16:40 +0400

    optima

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def moving_average(data, beta):
-    '''calculates the weighted moving average of a data set with bias correction'''
+    '''weighted moving average of a data set with bias correction'''
     if not isinstance(data, np.ndarray) or len(data.shape) != 1:
         return None
     if not isinstance(beta, float) or beta < 0 or beta >= 1:
