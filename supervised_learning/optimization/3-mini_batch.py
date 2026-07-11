8b29f08750fdf84808e9506cb4c298bf138a8ace
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-11 23:45:59 +0400

    optima

---
+++ supervised_learning/optimization/3-mini_batch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Optimization'''
+import numpy as np
+from 2-shuffle_data import shuffle_data
+
+
+def mini_batch(X, Y, batch_size):
+    '''creates mini-batches from two matrices'''
+    if X.shape[0] != Y.shape[0]:
+        return None, None
+    if batch_size <= 0 or batch_size > X.shape[0]:
+        return None, None
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
+    mini_batches_X = []
+    mini_batches_Y = []
+    for i in range(0, X.shape[0], batch_size):
+        if i + batch_size > X.shape[0]:
+            mini_batches_X.append(X_shuffled[i:X.shape[0]])
+            mini_batches_Y.append(Y_shuffled[i:Y.shape[0]])
+        else:
+            mini_batches_X.append(X_shuffled[i:i + batch_size])
+            mini_batches_Y.append(Y_shuffled[i:i + batch_size])
+    return mini_batches_X, mini_batches_Y
