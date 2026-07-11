76720e46564a5cddc69d2576d8d01b3e307b11fc
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-12 00:15:48 +0400

    optima

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -9,7 +9,7 @@ def moving_average(data, beta):
         return None
     if not isinstance(beta, float) or beta < 0 or beta >= 1:
         return None
-    moving_averages = []
+    moving_averages = [0] * data.shape[0]
     v = 0
     for i in range(data.shape[0]):
         v = beta * v + (1 - beta) * data[i]
