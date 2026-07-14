466bbee9230abfba5e708220be397faaa93e1781
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-15 01:05:56 +0400

    optima

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,10 +5,9 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, decay_step):
     """Updates the learning rate using inverse time decay in tensorflow."""
-    return tf.compat.v1.train.inverse_time_decay(
-        learning_rate=alpha,
-        global_step=tf.compat.v1.train.get_global_step(),
-        decay_steps=decay_step,
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        alpha=alpha,
+        decay_step=decay_step,
         decay_rate=decay_rate,
         staircase=True
     )
