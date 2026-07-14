4f90b65e6171eb6dd2423140c7904ed954c95f08
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-15 00:24:23 +0400

    optima

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Optimization'''
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    '''sets up the Adam algorithm in tensorflow, returns optimizer'''
+    return tf.keras.optimizers.Adam(learning_rate=alpha,
+                                    beta_1=beta1,
+                                    beta_2=beta2,
+                                    epsilon=epsilon)
