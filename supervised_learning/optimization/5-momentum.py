4624eba4a67d92a9792f17323c38c3929d556e6e
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-14 23:55:31 +0400

    optima

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -5,16 +5,6 @@ import numpy as np
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     '''updates a variable using the momentum algorithm'''
-    if not isinstance(alpha, float) or alpha < 0:
-        return None
-    if not isinstance(beta1, float) or beta1 < 0 or beta1 >= 1:
-        return None
-    if not isinstance(var, np.ndarray) or len(var.shape) != 1:
-        return None
-    if not isinstance(grad, np.ndarray) or grad.shape != var.shape:
-        return None
-    if not isinstance(v, np.ndarray) or v.shape != var.shape:
-        return None
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
     return var, v
