f1ee1ae9758f9a9215e08f44d1d824b38babc80a
İbrahim RAMAZANZADA <ibrahim.ramazanzada@tbtk.gov.tr>
2026-07-16 15:30:00 +0300

    regular show

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
-'''Regularization'''
+"""Forward propagation with dropout"""
+
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    '''Creates the forward propagation graph for the neural network
-       using dropout'''
+    """Creates the forward propagation graph using dropout"""
+
+    cache = {}
+    cache["A0"] = X
     A = X
-    for i in range(1, L):
-        Z = np.add(np.dot(A, weights['W' + str(i)]), weights['b' + str(i)])
-        A = np.tanh(Z)
-        D = np.random.binomial(1, keep_prob, size=A.shape)
-        A = A * D / keep_prob
-    ZL = np.add(np.dot(A, weights['W' + str(L)]), weights['b' + str(L)])
-    AL = np.softmax(ZL)
-    return AL
+
+    for i in range(1, L + 1):
+        W = weights["W{}".format(i)]
+        b = weights["b{}".format(i)]
+
+        Z = np.matmul(W, A) + b
+
+        if i == L:
+            t = np.exp(Z)
+            A = t / np.sum(t, axis=0, keepdims=True)
+        else:
+            A = np.tanh(Z)
+            D = np.random.binomial(1, keep_prob, size=A.shape)
+            A = (A * D) / keep_prob
+            cache["D{}".format(i)] = D
+
+        cache["A{}".format(i)] = A
+
+    return cache
