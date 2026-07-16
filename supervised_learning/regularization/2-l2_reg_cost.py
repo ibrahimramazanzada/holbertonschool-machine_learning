bdc9f258fc53da4aa43d47da2335a830b9b1f777
İbrahim RAMAZANZADA <ibrahim.ramazanzada@tbtk.gov.tr>
2026-07-16 15:04:20 +0300

    regular show

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Regularization'''
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    '''Calculates the cost of a neural network with L2 regularization'''
+    l2_cost = tf.add_n([tf.nn.l2_loss(var) for var in model.trainable_variables])
+    l2_cost *= (model.lambtha / 2)
+    return cost + l2_cost
