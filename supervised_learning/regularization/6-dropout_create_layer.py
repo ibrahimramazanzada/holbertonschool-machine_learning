d934b4d89be52557e91ee993158816caa4385ed4
İbrahim RAMAZANZADA <ibrahim.ramazanzada@tbtk.gov.tr>
2026-07-16 15:52:07 +0300

    regular show

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -10,7 +10,7 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     initializer = (tf.keras.initializers.VarianceScaling
                    (scale=2.0, mode=("fan_avg")))
     layer = tf.keras.layers.Dense(units=n, activation=activation,
-                                 kernel_initializer=initializer)
+                                  kernel_initializer=initializer)
     X = layer(prev)
     dropout_layer = tf.keras.layers.Dropout(1 - keep_prob)
 
