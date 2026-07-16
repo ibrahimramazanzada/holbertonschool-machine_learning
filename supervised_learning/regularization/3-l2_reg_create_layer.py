3346ad7f23ea87156bdac70533bee04217b5efe6
İbrahim RAMAZANZADA <ibrahim.ramazanzada@tbtk.gov.tr>
2026-07-16 15:18:28 +0300

    regular show

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,7 +7,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     '''Creates a tensorflow layer that includes L2 regularization'''
     kernel_regularizer = tf.keras.regularizers.l2(lambtha)
     kernel_initializer = (tf.keras.initializers.VarianceScaling
-                         (scale=2.0, mode=("fan_avg")))
+                          (scale=2.0, mode=("fan_avg")))
     return tf.keras.layers.Dense(
         units=n,
         activation=activation,
