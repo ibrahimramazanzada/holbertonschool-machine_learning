39415d6cf9d0df1daf76420e695a391972ede5c9
İbrahim RAMAZANZADA <ibrahim.ramazanzada@tbtk.gov.tr>
2026-07-16 13:07:48 +0300

    regular show

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -7,6 +7,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     '''Calculates the cost of a neural network with L2 regularization'''
     l2_cost = 0
     for i in range(1, L + 1):
-        l2_cost += np.sum(np.square(weights[i]))
+        l2_cost += np.sum(np.square(weights['W' + str(i)]))
     l2_cost *= (lambtha / (2 * m))
     return cost + l2_cost
