8ffc17340a7d3f218db9c67c9b020fb2b2cea74c
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-07-16 08:57:18 +0300

    optima
    Removed detailed docstring comments for batch_norm function.

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -4,15 +4,7 @@ import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    '''Batch normalization on a matrix Z.
-
-    Z: numpy.ndarray of shape (m, n)
-    gamma: numpy.ndarray of shape (1, n) or (n,)
-    beta: numpy.ndarray of shape (1, n) or (n,)
-    epsilon: small float added to variance for numerical stability
-
-    Returns: normalized Z of same shape as Z
-    '''
+    '''Batch normalization on a matrix Z.'''
     mu = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
     Z_norm = (Z - mu) / np.sqrt(var + epsilon)
