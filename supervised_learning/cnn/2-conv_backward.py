03b63578651e390345f4ca46a3ccdfade1f3fd75
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-14 19:51:57 +0400

    convolution

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -11,17 +11,31 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     sh, sw = stride
 
     if padding == "same":
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+        pad_h = max((h_new - 1) * sh + kh - h_prev, 0)
+        pad_w = max((w_new - 1) * sw + kw - w_prev, 0)
+
+        ph_before = pad_h // 2
+        ph_after = pad_h - ph_before
+
+        pw_before = pad_w // 2
+        pw_after = pad_w - pw_before
+
     elif padding == "valid":
-        ph, pw = 0, 0
+        ph_before = ph_after = 0
+        pw_before = pw_after = 0
+
     else:
         raise ValueError("padding must be 'same' or 'valid'")
 
     A_prev_pad = np.pad(
         A_prev,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant'
+        (
+            (0, 0),
+            (ph_before, ph_after),
+            (pw_before, pw_after),
+            (0, 0)
+        ),
+        mode="constant"
     )
 
     dA_prev_pad = np.zeros_like(A_prev_pad)
@@ -31,21 +45,20 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
     for i in range(h_new):
         for j in range(w_new):
+            vert_start = i * sh
+            vert_end = vert_start + kh
+
+            horiz_start = j * sw
+            horiz_end = horiz_start + kw
+
+            A_slice = A_prev_pad[
+                :,
+                vert_start:vert_end,
+                horiz_start:horiz_end,
+                :
+            ]
+
             for k in range(c_new):
-
-                vert_start = i * sh
-                vert_end = vert_start + kh
-
-                horiz_start = j * sw
-                horiz_end = horiz_start + kw
-
-                A_slice = A_prev_pad[
-                    :,
-                    vert_start:vert_end,
-                    horiz_start:horiz_end,
-                    :
-                ]
-
                 dz = dZ[:, i:i+1, j:j+1, k:k+1]
 
                 dA_prev_pad[
@@ -63,8 +76,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     if padding == "same":
         dA_prev = dA_prev_pad[
             :,
-            ph:-ph if ph != 0 else None,
-            pw:-pw if pw != 0 else None,
+            ph_before:ph_before + h_prev,
+            pw_before:pw_before + w_prev,
             :
         ]
     else:
