b644a516cec25570ee9b4c4edbc0cd1cd3eca71a
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-14 20:19:39 +0400

    convolution

---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""lenet5 architecture implementation"""
+from tensorflow import keras as K
+
+
+def lenet5(X):
+    """builds a modified version of the LeNet-5 architecture using keras"""
+
+    init = K.initializers.he_normal(seed=None)
+
+    model = K.Sequential()
+
+    model.add(K.layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation='relu',
+        kernel_initializer=K.initializers.he_normal(seed=0),
+        input_shape=X.shape[1:]
+    ))
+
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(K.layers.Conv2D(
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation='relu',
+        kernel_initializer=K.initializers.he_normal(seed=0)
+    ))
+
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(K.layers.Flatten())
+
+    model.add(K.layers.Dense(
+        units=120,
+        activation='relu',
+        kernel_initializer=init
+    ))
+
+    model.add(K.layers.Dense(
+        units=84,
+        activation='relu',
+        kernel_initializer=init
+    ))
+
+    model.add(K.layers.Dense(
+        units=10,
+        activation='softmax',
+        kernel_initializer=init
+    ))
+
+    return model
