dfb536c9728b22e117a3edc1e8b025eec137ffc8
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-16 21:39:20 +0400

    yolov3

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""yolov3 object detection model"""
+from tensorflow import Keras as K
+import numpy as np
+
+
+class Yolo:
+    """
+    Yolo class that uses the Yolo v3 algorithm to perform object detection
+    """
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Constructor method for the Yolo class"""
+
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
