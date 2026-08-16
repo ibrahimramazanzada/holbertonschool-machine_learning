07fc05c722dcb4f009efcad0b0d009eab7720f67
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-16 22:08:02 +0400

    yolov3

--- supervised_learning/object_detection/4-yolo.py
+++ supervised_learning/object_detection/4-yolo.py
@@ -159,3 +159,22 @@ class Yolo:
         union = box1_area + boxes_area - intersection
 
         return intersection / union
+
+    def load_images(self, folder_path):
+        """
+        Method that loads images from a folder
+        """
+        import os
+        import cv2
+
+        images = []
+        image_paths = []
+
+        for filename in os.listdir(folder_path):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                image_path = os.path.join(folder_path, filename)
+                image = cv2.imread(image_path)
+                images.append(image)
+                image_paths.append(image_path)
+
+        return images, image_paths
