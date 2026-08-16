e794d76738d2521518ef8d9e0d74816faa7ec163
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-16 23:04:05 +0400

    nst

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """neural style transfer"""
-from tensorflow import keras as K
-
+import tensorflow as tf
+import numpy as np
 
 class NST:
     """
@@ -15,11 +15,11 @@ class NST:
         """
         Constructor method for the NST class
         """
-        if (not isinstance(style_image, K.backend.floatx()) or
+        if (not isinstance(style_image, np.ndarray) or
            style_image.ndim != 3 or style_image.shape[2] != 3):
             raise TypeError("style_image must be a numpy.ndarray "
                             "of shape (h, w, 3)")
-        if (not isinstance(content_image, K.backend.floatx()) or
+        if (not isinstance(content_image, np.ndarray) or
            content_image.ndim != 3 or content_image.shape[2] != 3):
             raise TypeError("content_image must be a numpy.ndarray of"
                             " shape (h, w, 3)")
@@ -51,9 +51,8 @@ class NST:
         else:
             new_w = 512
             new_h = int(h * (512 / w))
-        resized_image = K.backend.resize_images(
-            image, new_h / h, new_w / w, "channels_last",
-            interpolation="bicubic")
+        resized_image = tf.image.resize(
+            image, [new_h, new_w], method='bicubic')
         # Scale pixel values to [0, 1]
         scaled_image = resized_image / 255.0
         return scaled_image
