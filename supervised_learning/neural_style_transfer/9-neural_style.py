42ff06d3b3071f41791a6083f8b03f1b99daf991
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-18 15:41:07 +0400

    nst

--- supervised_learning/neural_style_transfer/9-neural_style.py
+++ supervised_learning/neural_style_transfer/9-neural_style.py
@@ -204,3 +204,54 @@ class NST:
 
         grads = tape.gradient(J_total, generated_image)
         return grads, J_total, J_content, J_style
+
+    def generate_image(self, iterations=1000, step=None,
+                       lr=0.01, beta1=0.9, beta2=0.999):
+        """Method that generates the image"""
+
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be positive")
+
+        if step is not None and (not isinstance(step, int)):
+            raise TypeError("step must be an integer")
+        if step is not None and step <= 0 or step > iterations:
+            raise ValueError("step must be positive and less than iterations")
+
+        if not isinstance(lr, (int, float)):
+            raise TypeError("lr must be a number")
+        if lr <= 0:
+            raise ValueError("lr must be positive")
+
+        if not isinstance(beta1, float):
+            raise TypeError("beta1 must be a float")
+        if beta1 < 0 or beta1 > 1:
+            raise ValueError("beta1 must be in the range [0, 1]")
+
+        if not isinstance(beta2, float):
+            raise TypeError("beta2 must be a float")
+        if beta2 < 0 or beta2 > 1:
+            raise ValueError("beta2 must be in the range [0, 1]")
+
+        generated_image = tf.Variable(self.content_image, dtype=tf.float32)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=lr,
+                                             beta_1=beta1, beta_2=beta2)
+
+        best_cost = float('inf')
+        best_image = None
+        for i in range(iterations):
+            grads, J_total, J_content, J_style = self.compute_grads(
+                generated_image)
+            optimizer.apply_gradients([(grads, generated_image)])
+            generated_image.assign(tf.clip_by_value(generated_image, 0.0, 1.0))
+
+            if step is not None and (i + 1) % step == 0:
+                print(f"Cost at iteration {i + 1}: {J_total.numpy():.4e}, "
+                      f"content: {J_content.numpy():.4e}, "
+                      f"style: {J_style.numpy():.4e}")
+
+            if J_total < best_cost:
+                best_cost = J_total
+                best_image = generated_image.numpy()
+        return best_image, best_cost
