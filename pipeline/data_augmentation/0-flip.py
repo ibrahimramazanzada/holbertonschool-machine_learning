1eeb4d43d83abadad9d92d1416a034c4d4a96e86
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-23 23:14:17 +0400

    augmentation

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""flips an image horizontally"""
+"""
+Flip Image
+"""
 import tensorflow as tf
 
 
 def flip_image(image):
-    """flips an image horizontally"""
+    """
+    Flips an image horizontally.
+    """
     return tf.image.flip_left_right(image)
