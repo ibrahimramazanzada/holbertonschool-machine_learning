1d25c1e29e5f4022ff91bd8308659eab88a3b8ca
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-23 22:48:21 +0400

    t-sne

--- unsupervised_learning/dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/dimensionality_reduction/3-entropy.py
@@ -7,15 +7,13 @@ def HP(Di, beta):
     """Calculates the entropy of a distribution for a given precision"""
 
     # Compute the exponentials of the negative distances scaled by beta
-    P = np.exp(-Di * beta)
+    Pi = np.exp(-Di * beta)
 
-    # Normalize to get probabilities
-    sumP = np.sum(P)
-    if sumP == 0:
-        H = 0
-        P = np.zeros_like(P)
-    else:
-        P /= sumP
-        H = -np.sum(P * np.log2(P + 1e-10))  # Add small value to avoid log(0)
+    # Normalize to get the P affinities
+    sum_Pi = np.sum(Pi)
+    Pi = Pi / sum_Pi
 
-    return H, P
+    # Shannon entropy, base 2
+    Hi = -np.sum(Pi * np.log2(Pi))
+
+    return Hi, Pi
