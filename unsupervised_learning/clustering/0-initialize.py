40b4b8a6fba5394e5c3a836b5ee660a358264865
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-25 18:28:32 +0400

    kmeans

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -4,15 +4,17 @@ import numpy as np
 
 
 def initialize(X, k):
-    """Initialize cluster centroids for K-means"""
+    """
+    Initializes cluster centroids for K-means
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
-    if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
+    if not isinstance(k, int) or k <= 0:
         return None
 
-    m, n = X.shape
-    centroids = np.zeros((k, n))
-    random_indices = np.random.choice(m, size=k, replace=False)
-    centroids = X[random_indices]
+    n, d = X.shape
 
-    return centroids
+    low = X.min(axis=0)
+    high = X.max(axis=0)
+
+    return np.random.uniform(low, high, size=(k, d))
