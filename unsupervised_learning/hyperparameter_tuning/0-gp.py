b66a59f9f48f98a7f11286e88e3e0f903432d8b0
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-26 21:28:50 +0400

    gp

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Initialize Gaussian Process"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    Represents a noiseless 1D Gaussian process
+    """
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF)
+        """
+        sqdist = np.sum(X1 ** 2, axis=1).reshape(-1, 1) + \
+            np.sum(X2 ** 2, axis=1) - 2 * np.dot(X1, X2.T)
+
+        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
