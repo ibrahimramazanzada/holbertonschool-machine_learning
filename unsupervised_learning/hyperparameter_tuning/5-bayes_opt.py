f117f414aaf3a5db2c67238134de58d90387e1e3
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-26 22:05:08 +0400

    gp

---
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Bayesian Optimization"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """
+    Performs Bayesian optimization on a noiseless 1D Gaussian process
+    """
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Class constructor
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min_bound, max_bound = bounds
+        self.X_s = np.linspace(min_bound, max_bound,
+                                ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Calculates the next best sample location using Expected Improvement
+        """
+        mu, sigma = self.gp.predict(self.X_s)
+
+        if self.minimize:
+            Y_opt = np.min(self.gp.Y)
+            imp = Y_opt - mu - self.xsi
+        else:
+            Y_opt = np.max(self.gp.Y)
+            imp = mu - Y_opt - self.xsi
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            Z = imp / sigma
+
+        EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        EI[sigma == 0.0] = 0.0
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI
+
+    def optimize(self, iterations=100):
+        """
+        Optimizes the black-box function
+        """
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+
+            if X_next in self.gp.X:
+                break
+
+            Y_next = self.f(X_next)
+
+            self.gp.update(X_next, Y_next)
+
+        if self.minimize:
+            idx = np.argmin(self.gp.Y)
+        else:
+            idx = np.argmax(self.gp.Y)
+
+        X_opt = self.gp.X[idx]
+        Y_opt = self.gp.Y[idx]
+
+        return X_opt, Y_opt
