830f5424d21029153242114c1f25b8dbc1429992
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-28 18:13:07 +0400

    auto

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Vanilla autoencoder"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates an autoencoder"""
+
+    # Encoder
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    x = encoder_inputs
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+    encoder = keras.Model(encoder_inputs, latent)
+
+    # Decoder
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    x = decoder_inputs
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    decoder_outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(decoder_inputs, decoder_outputs)
+
+    # Full autoencoder
+    auto_inputs = keras.Input(shape=(input_dims,))
+    encoded = encoder(auto_inputs)
+    decoded = decoder(encoded)
+    auto = keras.Model(auto_inputs, decoded)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
