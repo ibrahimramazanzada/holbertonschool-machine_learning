1f81dbfb00a5b46937a96fb9aa31c24fa3b56036
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-08-31 18:02:52 +0400

    nlp

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,16 @@
+##!/usr/bin/env python3
+"""Tf-idf vectorizer"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Creates a TF-IDF embedding.
+    """
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+
+    embeddings = X.toarray()
+    features = vectorizer.get_feature_names_out()
+
+    return embeddings, features
