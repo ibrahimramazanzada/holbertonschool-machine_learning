07af6571c4c30c6516afd087178d7d79c8fbd54d
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 19:55:20 +0400

    q-learning

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
 """Perform Q-learning on a FrozenLake environment."""
 
 import numpy as np
@@ -42,3 +42,18 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1,
         )
 
     return Q, total_rewards
+
+load_frozen_lake = __import__('0-load_env').load_frozen_lake
+q_init = __import__('1-q_init').q_init
+import numpy as np
+
+np.random.seed(0)
+desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
+env = load_frozen_lake(desc=desc)
+Q = q_init(env)
+
+Q, total_rewards  = train(env, Q)
+print(Q)
+split_rewards = np.split(np.array(total_rewards), 10)
+for i, rewards in enumerate(split_rewards):
+    print((i+1) * 500, ':', np.mean(rewards))
