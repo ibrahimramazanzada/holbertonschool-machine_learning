67d78d650abbbc2a1eac269d30c8ce5c6b184760
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 22:28:33 +0400

    td

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
-"""Performs the Monte Carlo algorithm."""
-
+"""
+Monte Carlo Algorithm
+"""
 import numpy as np
 
 
-def monte_carlo(
-        env, V, policy, episodes=5000, max_steps=100,
-        alpha=0.1, gamma=0.99):
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
     """
-    Performs the Monte Carlo algorithm.
+    Monte Carlo algorithm to estimate the value function
     """
-    for ep in range(episodes):
-        state, _ = env.reset()
-        episode = []
+    for episode in range(episodes):
+        state = env.reset()[0]
+        episode_data = []
 
         for step in range(max_steps):
             action = policy(state)
-            step_result = env.step(action)
-            next_state, reward, terminated, truncated, info = step_result
-            episode.append((state, reward))
-            state = next_state
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            episode_data.append((state, reward))
 
             if terminated or truncated:
                 break
 
+            state = next_state
+
         G = 0
-        for state, reward in reversed(episode):
+        episode_data = np.array(episode_data, dtype=int)
+
+        for state, reward in reversed(episode_data):
             G = reward + gamma * G
-            V[state] = V[state] + alpha * (G - V[state])
+
+            if state not in episode_data[:episode, 0]:
+                V[state] += alpha * (G - V[state])
 
     return V
