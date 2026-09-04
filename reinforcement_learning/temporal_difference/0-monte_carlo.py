7db3dfd2aa8dc47422d31da3d190c28b05d92131
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 22:24:42 +0400

    td

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
-"""Perform the Monte Carlo algorithm."""
+"""Performs the Monte Carlo algorithm."""
 
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
-                alpha=0.1, gamma=0.99):
-    """Perform Monte Carlo learning and return the updated value estimate."""
-    for _ in range(episodes):
+def monte_carlo(
+        env, V, policy, episodes=5000, max_steps=100,
+        alpha=0.1, gamma=0.99):
+    """
+    Performs the Monte Carlo algorithm.
+    """
+    for ep in range(episodes):
         state, _ = env.reset()
-        states = []
-        rewards = []
+        episode = []
 
-        for _ in range(max_steps):
+        for step in range(max_steps):
             action = policy(state)
-            new_state, reward, terminated, truncated, _ = env.step(action)
-
-            states.append(state)
-            rewards.append(reward)
-
-            state = new_state
+            step_result = env.step(action)
+            next_state, reward, terminated, truncated, info = step_result
+            episode.append((state, reward))
+            state = next_state
 
             if terminated or truncated:
                 break
 
-        for i in range(len(states)):
-            G = 0
-
-            for j in range(i, len(rewards)):
-                G += gamma ** (j - i) * rewards[j]
-
-            state = states[i]
-
+        G = 0
+        for state, reward in reversed(episode):
+            G = reward + gamma * G
             V[state] = V[state] + alpha * (G - V[state])
 
     return V
