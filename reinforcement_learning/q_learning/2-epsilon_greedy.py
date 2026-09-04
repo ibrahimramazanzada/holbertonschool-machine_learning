b6b6492be78300060d720b93ab59d544448f2b47
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 19:51:38 +0400

    q-learning

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Select an action using the epsilon-greedy algorithm."""
+
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """Determine the next action using epsilon-greedy."""
+    p = np.random.uniform()
+
+    if p < epsilon:
+        return np.random.randint(Q.shape[1])
+
+    return np.argmax(Q[state])
