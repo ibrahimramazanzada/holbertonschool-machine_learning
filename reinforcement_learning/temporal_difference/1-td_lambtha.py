598007a57b24a3a98d47b166c0028cc2d2a07ba5
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 23:21:28 +0400

    td

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
-                alpha=0.1, gamma=0.99):
+               alpha=0.1, gamma=0.99):
     """Perform the TD(λ) algorithm to estimate a value function.
 
     Args:
