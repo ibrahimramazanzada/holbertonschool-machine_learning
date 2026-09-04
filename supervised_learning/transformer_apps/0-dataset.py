261227e0527e292ffac7a02ab4feda619f3a9e94
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-09-04 18:16:10 +0400

    transformer

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Defines the Dataset class for machine translation preprocessing."""
+import tensorflow_datasets as tfds
+import transformers
+
+
+class Dataset:
+    """Loads and preps the pt->en translation dataset for training."""
+
+    def __init__(self):
+        """Initialize train/validation splits and sub-word tokenizers."""
+        self.data_train = self.load_pt2en('train')
+        self.data_valid = self.load_pt2en('validation')
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def load_pt2en(self, split):
+        """
+        Load the ted_hrlr_translate/pt_to_en dataset for a given split.
+        """
+        return tfds.load('ted_hrlr_translate/pt_to_en',
+                          split=split, as_supervised=True)
+
+    def tokenize_dataset(self, data):
+        """
+        Create sub-word tokenizers for the Portuguese/English dataset.
+        """
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data.as_numpy_iterator():
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
+
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased')
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased')
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            pt_sentences, vocab_size=2 ** 13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            en_sentences, vocab_size=2 ** 13)
+
+        return tokenizer_pt, tokenizer_en
